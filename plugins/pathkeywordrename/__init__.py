fec5995ee99e67f569e8dc6dae85e39c91283073
shanhai <1195585382@qq.com>
2025-08-25 17:27:20 +0800

    add:路径关键字重命名

--- plugins/pathkeywordrename/__init__.py
+++ plugins/pathkeywordrename/__init__.py
@@ -19,7 +19,7 @@ class PathKeywordRename(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/smartrename.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "shanhai2333"
     # 作者主页
