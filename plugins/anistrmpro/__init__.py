65f16bd70619fd9501a31943314d08d89e4355d7
shanhai <1195585382@qq.com>
2026-03-06 17:01:27 +0800

    edit： 回退

--- plugins/anistrmpro/__init__.py
+++ plugins/anistrmpro/__init__.py
@@ -58,7 +58,7 @@ class ANiStrmPro(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/shanhai2333/MoviePilot-Plugins/main/icons/anistrmpro.png"
     # 插件版本
-    plugin_version = "2.8.2"
+    plugin_version = "2.8.5"
     # 插件作者
     plugin_author = "honue,shanhai2333"
     # 作者主页
